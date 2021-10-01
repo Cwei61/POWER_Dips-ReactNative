21036bd1813ada0febcb3726f989779235fae2d0
albert9052 <35403151+albert9052@users.noreply.github.com>
2021-10-01 21:20:18 +0800

    1. Added react-native-progress-steps.

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+      <ProgressSteps>
+        <ProgressStep label="First Step">
+            <View style={{ alignItems: 'center' }}>
+                <Text>This is the content within step 1!</Text>
+            </View>
+        </ProgressStep>
+        <ProgressStep label="Second Step">
+            <View style={{ alignItems: 'center' }}>
+                <Text>This is the content within step 2!</Text>
+            </View>
+        </ProgressStep>
+        <ProgressStep label="Third Step">
+            <View style={{ alignItems: 'center' }}>
+                <Text>This is the content within step 3!</Text>
+            </View>
+        </ProgressStep>
+      </ProgressSteps>
     </View>
   );
 }
